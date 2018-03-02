594920741da39f280cc3c33eda62a33893ea1f98
1not <toni01110000@gmail.com>
2018-03-03 00:09:59 +0100

    go to top button

---
+++ js/top.js
@@ -0,0 +1,27 @@
+window.onscroll = function() {scrollFunction()};
+
+
+function scrollFunction()
+{
+    if (document.body.scrollTop > 260 || document.documentElement.scrollTop > 260)
+    {
+        document.getElementById("button_top").style.display = "block";
+    }
+    else
+    {
+        document.getElementById("button_top").style.display = "none";
+    }
+}
+
+
+var timeOut;
+
+function scrollToTop()
+{   
+	if (document.body.scrollTop != 0 || document.documentElement.scrollTop != 0)
+    {
+		window.scrollBy(0, -50);
+		timeOut = setTimeout('scrollToTop()', 8);
+	}
+	else clearTimeout(timeOut);
+}
