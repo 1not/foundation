76bc481842f6d4759e668242f8a9b5b73105b29a
1not <toni01110000@gmail.com>
2018-03-04 17:47:49 +0100

    add calendar to events page

--- js/top.js
+++ js/top.js
@@ -21,7 +21,7 @@ function scrollToTop()
 	if (document.body.scrollTop != 0 || document.documentElement.scrollTop != 0)
     {
 		window.scrollBy(0, -50);
-		timeOut = setTimeout('scrollToTop()', 8);
+		timeOut = setTimeout('scrollToTop()', 5);
 	}
 	else clearTimeout(timeOut);
 }
